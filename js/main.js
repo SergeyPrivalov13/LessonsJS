48f139b2f6ca72230c88ad46c829b32de201b4d8
Sergey Privalov <SergeyPrivalof@yandex.ru>
2020-01-25 12:53:45 +0500

    Выполнен Урок №2

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
-let money,
-  income,
-  addExpenses,
-  deposit,
-  mission,
-  period;
+let money = 50000,                                    // Доход за месяц
+  income = 'фрилансер',                               // Дополнительный доход
+  addExpenses = 'Интернет, Такси, Коммуналка',        // Дополнительные расходы
+  deposit = true,
+  mission = 20000,                                    // Сумма накоплений
+  period = 4,
+  budgetDay = money / 30;                             // Дневной бюджет
 
-alert('Hello');
-console.log('Hello World');
+console.log(typeof (money));
+console.log(typeof (income));
+console.log(typeof (deposit));
+
+console.log(addExpenses.length);
+
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей/долларов/гривен/юани');
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+console.log(budgetDay);
+
