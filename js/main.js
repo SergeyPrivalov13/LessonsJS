48c165cf258b9e068533d0eae4917f2c7a531374
Sergey Privalov <SergeyPrivalof@yandex.ru>
2020-01-29 16:49:31 +0500

    Выполнен Урок №5 пункт 2 - усложнённое

--- js/main.js
+++ js/main.js
@@ -1,22 +1,24 @@
 'use strict';
-let v1 = "   Eveniet omnis repellat  ",
-    v2 = 2,
-    v3 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet omnis repellat dignissimos nisi officia sapiente necessitatibus alias voluptate assumenda incidunt quasi, esse sequi architecto blanditiis porro saepe! Maiores numquam nesciunt architecto mollitia accusamus totam sint! Laudantium nemo asperiores itaque pariatur?",
-    v4 = null;
+let arr = ['75632', '867476', '99682', '22274', '36958', '8711', '0492'];
 
-function text (val) {
-  let textStr = typeof val;                            /* Опредиляем тип */
-  if ( textStr === 'string') {                         /* Если тип Строка */
-    val = val.replace(/^\s*/,'').replace(/\s*$/,'');   /* Удаляем пробелы в начале и конце строки */
-    if (val.length >= 30) {                            /* Опредиляем количество символов */
-      val = val.slice(0, 30) + " ...";                 /* Если символов > 30 */
-      return (val);      
-    } else {
-      return val;
-    }
-  } else {
-    return ('Это не строка');                           /* Если тип не строка */
+if (arr.length > 0){  
+  for(let i = 0; i < arr.length; i++){
+    let num = arr[i].split('');
+    console.log(num);
+    
+    
   }  
+}else {
+  console.log('Массив пуст');  
 }
 
-console.log(">" + text(v3) + "<");
+
+// Задание 2
+let n = 100;
+nextPrime:
+for (let i = 2; i <= n; i++) { 
+  for (let j = 2; j < i; j++) { 
+    if (i % j === 0) {continue nextPrime;} 
+  }
+  console.log(`Делители числа ${i}: 1 и ${i}`); // простое число
+}
