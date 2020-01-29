994e3269bdea6ebf237eaf6bd90a3e7a6a300af2
Sergey Privalov <SergeyPrivalof@yandex.ru>
2020-01-29 14:58:31 +0500

    Выполнен Урок 5

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 'use strict';
 
-const
-  // Доход за месяц
-  money = +prompt('Ваш месячный доход?', 50000),
+let isNumber = function(n){
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+const  
   // Дополнительный доход                       
   income = 'фрилансер',                                           
   // Дополнительные расходы
@@ -10,16 +12,19 @@ const
   // Депозит
   deposit = confirm('Есть ли у вас депозит в банке?'),            
   // Сумма накоплений                                               
-  mission = 100000,
+  mission = 100000;
   // Обязательные рассходы 1    
-  expenses1 = prompt('Введите обязательную статью расходов?', 'садик государственный' ),
+let 
+  // Доход за месяц
+  money,
+  // Обязательные рассходы 1
+  expenses1,
   // Сумма обязательных расходов 1                 
-  amount1 = +prompt('Во сколько это обойдется?', 2500),
+  //amount1 = +prompt('Во сколько это обойдется?', 2500),
   // Обязательные рассходы 2    
-  expenses2 = prompt('Введите обязательную статью расходов?','садик частный'),
+  expenses2;
   // Сумма обязательных расходов 2                 
-  amount2 = +prompt('Во сколько это обойдется?', 2500);
-                        
+  //amount2 = +prompt('Во сколько это обойдется?', 2500);                        
 
 //функция для склонения числительный
 // n - число
@@ -28,6 +33,13 @@ const declOfNum = (n, t) => t[ (n%100>4 && n%100<20)? 2 : [2, 0, 1, 1, 1, 2][(n%
   month = ['месяц', 'месяца', 'месяцев'];
 const declOfMon = (n, t) => t[ (n%100>4 && n%100<20)? 2 : [2, 0, 1, 1, 1, 2][(n%10<5)?n%10:5] ],
   moneys = ['рубль', 'рубля', 'рублей'];
+
+const start = function(){
+  do {
+    money = prompt('Ваш месячный доход?');    
+  } while (!isNumber(money));
+};
+start();
 
 const showTypeOf = function(data){
   console.log(data, typeof(data));  
@@ -38,9 +50,26 @@ showTypeOf(deposit);
 
 // Расходы за месяц
 const getExpensesMonth = function(){
-  return amount1 + amount2;
+  let sum = 0;
+
+  for (let i = 0; i < 2; i++){
+    if (i === 0) {
+      expenses1 = prompt('Введите обязательную статью расходов?', 'садик государственный' );
+    } else if (i === 1){
+      expenses2 = prompt('Введите обязательную статью расходов?','садик частный');
+    }
+    let count;
+    do {
+      count = prompt('Во сколько это обойдется?');
+    } while (!isNumber(count));
+
+    sum += +count;    
+  }
+
+  return sum;
 };
-console.log(`Расходы за месяц: ${getExpensesMonth()} ${declOfMon(getExpensesMonth(), moneys)}`);
+let expensesMonth = getExpensesMonth();
+console.log(`Расходы за месяц: ${expensesMonth} ${declOfMon(expensesMonth, moneys)}`);
 
 // Вывод возможных расходов в виде массива
 console.log(addExpenses.toLowerCase().split(/,\s*/));
@@ -48,7 +77,7 @@ console.log(addExpenses.toLowerCase().split(/,\s*/));
 // Функция возвращает накопления за месяц
 const getAccumulatedMonth = function (){
   // Объявляем переменную и присваиваем ей резултьтат
-  let accumulatedMonth = money - getExpensesMonth();
+  let accumulatedMonth = money - expensesMonth;
   return accumulatedMonth;
 };
 console.log(`Накопления за месяц: ${getAccumulatedMonth()} ${declOfMon(getAccumulatedMonth(), moneys)}`);
@@ -61,9 +90,14 @@ console.log(`Бюджет на день: ${getBudgetDay()} ${declOfMon(getBudget
 
 // Cрок достижения цели в месяцах
 const getTargetMonth = function(){
-  return Math.ceil(mission / getAccumulatedMonth());
+  let num = Math.ceil(mission / getAccumulatedMonth());
+  if (num >= 0){
+    return `${num} ${declOfNum(num, month)}`;
+  } else {
+    return 'Цель не будет достигнута';
+  }
 }; 
-console.log(`Cрок достижения цели в месяцах: ${getTargetMonth()} ${declOfNum(getTargetMonth(), month)}`);
+console.log(`Cрок достижения цели в месяцах: ${getTargetMonth()}`);
 
 // Уровень дохода
 const getStatusIncome = function(){
@@ -79,3 +113,8 @@ const getStatusIncome = function(){
   }
 };
 console.log(getStatusIncome());
+
+
+console.log(`Доход за месяц: ${money}`);
+console.log(`Цель: ${mission}`);
+
