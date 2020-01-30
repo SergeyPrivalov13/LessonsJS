f97d2b8a5761d07e98f8087ed56233dcc4db92cc
Sergey Privalov <SergeyPrivalof@yandex.ru>
2020-01-30 23:13:08 +0500

    Наработки по уроку 6

--- js/main.js
+++ js/main.js
@@ -1,19 +1,86 @@
 'use strict';
-function random(a){
-  // Проверка число или нет
+/* function funcMath(a) {
+  // Задаём переменную
+  //const a = 10;
+  // Возвращаем результат функцию
+  return function(b) {
+    console.log(a * b);    
+  };
+}
+
+// Переменная которая вызывает функцию 
+// и результат сохраняет в себе
+
+const mathPow = funcMath(10);
+
+// Получаю результат работы функции
+mathPow(2); */
+  
+  /* let str = prompt;
+  function name(val) {
+    return function (str) {
+      if(isNaN(str)) {          
+        str = prompt;
+        return temp(str);
+      }
+    };
+  }
+  const temp = name(24);
+  temp(str); */
+
+/* let isNumber = function(n){
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+let random = 10,
+  numberUser = prompt('Угадай число от 1 до 100');
+
+if (Boolean(numberUser) === true ){
+  console.log('Что то ввели');
+  numberUser = numberUser.trim();
+  if(numberUser === ''){
+    console.log('Пустая строка'); 
+  } else {
+    console.log('Ввели даные');  
+    if(+numberUser > random) {
+      console.log('Загаданное число меньше'); 
+    } else if(+numberUser < random) {
+      console.log('Загаданное число больше');  
+    } else{
+      console.log('Вы угадали!!!');
+    }    
+  }
+} else {  
+  if(numberUser === null){
+    console.log('Нажали Отмена');  
+  } else {
+    console.log('Нажали OK с пустой строкой');  
+  }  
+} */
+
+
+let question;
+function check(val){
   let isNumber = function(n){
     return !isNaN(parseFloat(n)) && isFinite(n);
   };
 
-  do {
-    a = prompt('Угадай число от 1 до 100');
+  const start = function(){
+    do {
+      val = prompt('Угадай число от 1 до 100');    
+    } while (!isNumber(val));
     
-  } while (!isNumber(a));
-
+  };
+  start();
+  console.log(val);
   
 
-  console.log(a);
-  console.log(typeof(a));
+
+
+  
+  return val;
+  
 
 }
-random();
+console.log(check(question));
+
