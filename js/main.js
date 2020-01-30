1a14da15e057aa2cb1b03b4aafe1ca977721ea8a
Sergey Privalov <SergeyPrivalof@yandex.ru>
2020-01-30 16:47:07 +0500

    добавил функционал

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
 'use strict';
+function random(a){
+  // Проверка число или нет
+  let isNumber = function(n){
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  };
 
+  do {
+    a = prompt('Угадай число от 1 до 100');
+    
+  } while (!isNumber(a));
+
+  
+
+  console.log(a);
+  console.log(typeof(a));
+
+}
+random();
