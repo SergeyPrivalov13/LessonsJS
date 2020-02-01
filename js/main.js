a40ef80bc859d6a115f36099bcf545da930476e2
Sergey Privalov <SergeyPrivalof@yandex.ru>
2020-02-02 01:19:04 +0500

    начало

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
 'use strict';
+let week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'], 
+  date = new Date(),
+  presentDay = date.getDay(),
+  blockWeek = document.getElementById('week');
+
+for(let i = 0; i < week.length; i++){
+  console.log(week[i]);
+  blockWeek = blockWeek.innerHTML(week[i]);
+  
+}
+
+console.log(date);
+console.log(presentDay);
+console.log(week);
+console.log(blockWeek);
