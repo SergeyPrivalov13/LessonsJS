0f5c53d4535e08abb66854a2943242b1baaf501a
Sergey Privalov <SergeyPrivalof@yandex.ru>
2020-02-06 01:04:07 +0500

    Подправил вывод дня недели и месяца

--- js/main.js
+++ js/main.js
@@ -8,49 +8,25 @@ function timer(){
     month = dateNow.getMonth(),                                       // Месяц
     monthTwo = (`0${dateNow.getMonth()}`).replace(/.?(\d{2})/,'$1'),  // Месяц
     day = (`0${dateNow.getDate()}`).replace(/.?(\d{2})/,'$1'),        // Число
-    weekDay = dateNow.getDay(),                                       // День недели
+    presentDay = dateNow.getDay(),                                    // День недели
     hour = (`0${dateNow.getHours()}`).replace(/.?(\d{2})/,'$1'),      // Час
     minutes = (`0${dateNow.getMinutes()}`).replace(/.?(\d{2})/,'$1'), // Минуты
     seconds = (`0${dateNow.getSeconds()}`).replace(/.?(\d{2})/,'$1'), // Секунды
-    fMonth,                                                           // Название месяца
-    fday;                                                             // Название дня
+    week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
+    monthName = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня',
+    'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'];
 
+  const declOfNum = (n, t) => t[ (n%100>4 && n%100<20)? 2 : [2, 0, 1, 1, 1, 2][(n%10<5)?n%10:5] ];  
 
-  const declOfNum = (n, t) => t[ (n%100>4 && n%100<20)? 2 : [2, 0, 1, 1, 1, 2][(n%10<5)?n%10:5] ],
-    decl = [];
-  
-  
-
-  switch (month)
-    {
-      case 0: fMonth="января"; break;
-      case 1: fMonth="февраля"; break;
-      case 2: fMonth="марта"; break;
-      case 3: fMonth="апреля"; break;
-      case 4: fMonth="мае"; break;
-      case 5: fMonth="июня"; break;
-      case 6: fMonth="июля"; break;
-      case 7: fMonth="августа"; break;
-      case 8: fMonth="сентября"; break;
-      case 9: fMonth="октября"; break;
-      case 10: fMonth="ноября"; break;
-      case 11: fMonth="декабря"; break;
-    }
-  
-  switch (weekDay - 1)
-    {
-      case 0: fday="Понедельник"; break;
-      case 1: fday="Вторник"; break;
-      case 2: fday="Среда"; break;
-      case 3: fday="Четверг"; break;
-      case 4: fday="Пятница"; break;
-      case 5: fday="Суббота"; break;
-      case 6: fday="Воскресенье"; break;
-    }
+  if(presentDay === 0){
+    presentDay = 6;
+  } else {
+    presentDay = presentDay - 1;
+  }  
   
   date.style.cssText = 'color: red; font-weight: bold';
   dateTwo.style.cssText = 'color: red; font-weight: bold';
-  date.innerHTML = `Сегодня ${fday}, ${day} ${fMonth} ${year} года, 
+  date.innerHTML = `Сегодня ${week[presentDay]}, ${day} ${monthName[month]} ${year} года, 
   ${hour} ${declOfNum(hour, ['час', 'часа', 'часов'])} 
   ${minutes} ${declOfNum(minutes, ['минута', 'минуты', 'минут'])}
   ${seconds} ${declOfNum(seconds, ['секунда', 'секунды', 'секунд'])}`;
