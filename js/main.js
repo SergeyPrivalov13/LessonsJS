18596c608b44609d36f7112a1b05f563236dc9bc
Sergey Privalov <SergeyPrivalof@yandex.ru>
2020-02-20 19:49:37 +0500

    Выполнен Урок 19 Усложнённое

--- js/main.js
+++ js/main.js
@@ -59,27 +59,25 @@ document.addEventListener('DOMContentLoaded', () => {
   // Меню
   const toggleMenu = () => {
     const
-      // Кнопка Меню
-      btnMenu = document.querySelector('.menu'),
       // Блок с меню
-      menu = document.querySelector('menu');
-    
+      menu = document.querySelector('menu'),
+      body = document.querySelector('body');
+
     // Функция отселживает translateX
     const handlerMenu = () => {
       // Добавляем/Убираем класс active-menu
       menu.classList.toggle('active-menu');
     };
-      
-    // Клик по кнопке меню
-    btnMenu.addEventListener('click', handlerMenu);
     
-    // Отслеживаем нажатие на блок с меню 
-    menu.addEventListener('click', (event) => {
+    body.addEventListener('click', (event) => {
       let target = event.target;
-      // Если нажали элемент с классом close-btn(крестик) - закрываем окно
-      if(target.classList.contains('close-btn')){
-        handlerMenu();
-      } else {
+      // Показать меню если кликнули по кнопке Меню
+      if(target.closest('.menu')){
+        handlerMenu();        
+      } // Убрать меню если кликнули на крестик или вне меню
+      else if (target.closest('.close-btn') || !target.closest('menu')){
+        menu.classList.remove('active-menu'); 
+      } else{
         // Иначе target присваиваем тэг li
         target = target.closest('li');
         // Если нажали на li закрываем окно
