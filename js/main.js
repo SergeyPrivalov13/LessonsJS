d2c2bc0d7a52b0983b35e82d1ad48977008d0286
Sergey Privalov <SergeyPrivalof@yandex.ru>
2020-02-25 15:46:55 +0500

    Выполнен урок по видео

--- js/main.js
+++ js/main.js
@@ -450,19 +450,82 @@ document.addEventListener('DOMContentLoaded', () => {
   };
   replaceImage();
 
-  // Валидация калькулятора
-  const validationCalculator = () => {
-    const 
-    calcBlock = document.querySelector('.calc-block'),
-    calcItem = calcBlock.querySelectorAll('input');
-    console.log(calcItem.value);
-    calcItem.forEach((elem) => {
-      elem.addEventListener('input', () => {
-        elem.value = elem.value.replace(/\D/g, '');  
-      });
-    });  
+  // Калькулятор расчёта стоимости
+  const calculator = (price = 100) => {
+    const
+      // Блок со всеми полями 
+      calcBlock = document.querySelector('.calc-block'),
+      // Поле Select
+      calcType = document.querySelector('.calc-type'),
+      // Поле Общая площадь*
+      calcSquare = document.querySelector('.calc-square'),
+      // Поле Количество помещений
+      calcСount = document.querySelector('.calc-count'),
+      // Поле Срок исполнения
+      calcDay = document.querySelector('.calc-day'),
+      // Результат
+      total = document.getElementById('total'),
+      // Поля ввода
+      calcItem = calcBlock.querySelectorAll('input');
+
+    // Функция подсчёта итоговой цены
+    const countSum = () => {
+      const 
+        // Значение value у Select -> Option
+        typeValue = calcType.options[calcType.selectedIndex].value,
+        // Вычисление площади
+        squareValue = +calcSquare.value;
+      // Результата по умолчанию
+
+      let result = 0,
+        // Количество помещений по умолчанию
+        countValue = 1,
+        // Срок исполнения по умолчанию
+        dayValue = 1;
+
+      // Условие для кол-ва помещений
+      if (calcСount.value > 1) {
+        countValue += (calcСount.value - 1) / 10;
+      }
+
+      // Условие для срока исполнения
+      if(calcDay.value && calcDay.value <= 5){
+        dayValue *= 2;  
+      } else if (calcDay.value && calcDay.value <= 10){
+        dayValue *= 1.5;  
+      }
+
+      // Условие на заполненость полей 
+      if(typeValue && squareValue){
+        // Вычисляем результат
+        result = price * typeValue * squareValue * countValue * dayValue;
+      }
+
+      // Выводим результат на страницу
+      total.textContent = result;
+    };
+
+    // Отлавливаем изменение в блоке
+    calcBlock.addEventListener('change', (event) => {
+      let target = event.target;      
+      // Проверяем были ли изминения в полях
+      if(target.matches('select') || target.matches('input')){
+        countSum();  
+      }
+    });
+
+    // Валидация калькулятора
+    const validationCalculator = () => {
+      calcItem.forEach((elem) => {
+        elem.addEventListener('input', () => {
+          elem.value = elem.value.replace(/\D/g, '');  
+        });
+      });  
+    };
+    validationCalculator();
   };
-  validationCalculator();
+  calculator(100);
+
 
 
 
